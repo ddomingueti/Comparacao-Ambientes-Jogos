2b205274a83669601dd5772b4d30724fa5415394
ddomingueti <32684668+ddomingueti@users.noreply.github.com>
2018-01-13 12:13:28 -0200

    Adicionados os projetos da Unity 3

---
+++ Unity/4 - Erro Jokenpo/Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+
+	public void Jogar() {
+		SceneManager.LoadScene ("Jogo");
+	}
+
+	public void Sair() {
+		Application.Quit ();
+	}
+}
